59dbd12461e07da0305b7c448103d594abca3c7e
Felipe Nogueira <felipenogueira.94@gmail.com>
2023-02-27 20:01:55 -0300

    estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { HashRouter } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
+import { Router } from './Router'
+import { defaultTheme } from './styles/theme/defaultTheme'
+import { GlobalStyles } from './styles/theme/global'
+
 export function App() {
   return (
     <div>
-
+      <HashRouter>
+        <ThemeProvider theme={defaultTheme}>
+          <GlobalStyles />
+          <Router />
+        </ThemeProvider>
+      </HashRouter>
     </div>
   )
 }
