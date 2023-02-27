883e1a0abb522e3463a571dfca6a031e1e1d415a
Felipe Nogueira <felipenogueira.94@gmail.com>
2023-02-27 20:18:11 -0300

    componente header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import logoSrc from '../../assets/logo.svg'
+import { HeaderContainer } from "./styles";
+
+export function Header() {
+    return (
+        <HeaderContainer>
+            <img src={logoSrc} alt="" />
+        </HeaderContainer>
+    )
+}
